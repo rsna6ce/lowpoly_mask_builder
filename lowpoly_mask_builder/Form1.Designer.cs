d4a11259a7ed9e9dfbc1dfbe836fdd0d6aa82204
rsna6ce <86136223+rsna6ce@users.noreply.github.com>
2025-11-29 18:55:45 +0900

    added step1.

---
+++ lowpoly_mask_builder/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace lowpoly_mask_builder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxLeft = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBoxRight = new System.Windows.Forms.PictureBox();
+            this.vScrollBarZ = new System.Windows.Forms.VScrollBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxLeft
+            // 
+            this.pictureBoxLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBoxLeft.Location = new System.Drawing.Point(4, 28);
+            this.pictureBoxLeft.Name = "pictureBoxLeft";
+            this.pictureBoxLeft.Size = new System.Drawing.Size(600, 788);
+            this.pictureBoxLeft.TabIndex = 0;
+            this.pictureBoxLeft.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1286, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // pictureBoxRight
+            // 
+            this.pictureBoxRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBoxRight.Location = new System.Drawing.Point(604, 28);
+            this.pictureBoxRight.Name = "pictureBoxRight";
+            this.pictureBoxRight.Size = new System.Drawing.Size(600, 788);
+            this.pictureBoxRight.TabIndex = 0;
+            this.pictureBoxRight.TabStop = false;
+            this.pictureBoxRight.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxRight_Paint);
+            this.pictureBoxRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxRight_MouseDown);
+            this.pictureBoxRight.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxRight_MouseMove);
+            this.pictureBoxRight.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxRight_MouseUp);
+            // 
+            // vScrollBarZ
+            // 
+            this.vScrollBarZ.Location = new System.Drawing.Point(1228, 32);
+            this.vScrollBarZ.Name = "vScrollBarZ";
+            this.vScrollBarZ.Size = new System.Drawing.Size(24, 780);
+            this.vScrollBarZ.TabIndex = 2;
+            this.vScrollBarZ.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBarZ_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 820);
+            this.Controls.Add(this.vScrollBarZ);
+            this.Controls.Add(this.pictureBoxRight);
+            this.Controls.Add(this.pictureBoxLeft);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxLeft;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBoxRight;
+        private System.Windows.Forms.VScrollBar vScrollBarZ;
+    }
+}
+
---
+++ lowpoly_mask_builder/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace lowpoly_mask_builder
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private List<Vertex> vertices = new List<Vertex>();
+        private List<Triangle> triangles = new List<Triangle>();
+        private Vertex selectedVertex = null;
+        private bool isDragging = false;
+        private Point dragStartPos;
+
+        // 定数
+        private const int GRID_SIZE = 1;
+        private const int POINT_RADIUS = 4;
+        private const int EDGE_ACTIVE_DISTANCE = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeModel();
+        }
+
+        private void InitializeModel()
+        {
+            // 初期状態：(0,0), (100,0), (0,100)の三角形を作成
+            vertices.Clear();
+            triangles.Clear();
+
+            vertices.Add(new Vertex(0, 0));   // 頂点0
+            vertices.Add(new Vertex(100, 0)); // 頂点1
+            vertices.Add(new Vertex(0, 100)); // 頂点2
+
+            triangles.Add(new Triangle(0, 1, 2));
+
+            pictureBoxRight.Invalidate();
+        }
+
+        private void pictureBoxRight_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.LightCyan);
+
+            // グリッドを描画
+            DrawGrid(g);
+
+            // 三角形を描画
+            foreach (var triangle in triangles)
+            {
+                DrawTriangle(g, triangle);
+            }
+
+            // 頂点を描画
+            foreach (var vertex in vertices)
+            {
+                DrawVertex(g, vertex, vertex == selectedVertex);
+            }
+        }
+
+        private void DrawGrid(Graphics g)
+        {
+            Pen gridPen = new Pen(Color.FromArgb(200, 200, 200));
+            int width = pictureBoxRight.Width;
+            int height = pictureBoxRight.Height;
+
+            for (int x = 0; x < width; x += 10)
+                g.DrawLine(gridPen, x, 0, x, height);
+            for (int y = 0; y < height; y += 10)
+                g.DrawLine(gridPen, 0, y, width, y);
+        }
+
+        private void DrawTriangle(Graphics g, Triangle triangle)
+        {
+            Point p1 = WorldToScreen(vertices[triangle.V1]);
+            Point p2 = WorldToScreen(vertices[triangle.V2]);
+            Point p3 = WorldToScreen(vertices[triangle.V3]);
+
+            Pen pen = new Pen(Color.Black, 1.5f);
+            g.DrawLine(pen, p1, p2);
+            g.DrawLine(pen, p2, p3);
+            g.DrawLine(pen, p3, p1);
+        }
+
+        private void DrawVertex(Graphics g, Vertex vertex, bool isSelected)
+        {
+            Point screenPos = WorldToScreen(vertex);
+            int radius = isSelected ? POINT_RADIUS + 2 : POINT_RADIUS;
+            Brush brush = isSelected ? Brushes.Red : Brushes.Blue;
+
+            g.FillEllipse(brush, screenPos.X - radius, screenPos.Y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(Pens.Black, screenPos.X - radius, screenPos.Y - radius, radius * 2, radius * 2);
+        }
+
+        private Point WorldToScreen(Vertex vertex)
+        {
+            return new Point(vertex.X, vertex.Y);
+        }
+
+        private Vertex ScreenToWorld(Point screenPoint)
+        {
+            // 画面座標をワールド座標に変換（現在は1:1）
+            return new Vertex(screenPoint.X, screenPoint.Y);
+        }
+
+        private void pictureBoxRight_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // 頂点の選択またはドラッグ開始
+                Vertex nearestVertex = FindNearestVertex(e.Location, 10);
+                if (nearestVertex != null)
+                {
+                    selectedVertex = nearestVertex;
+                    isDragging = true;
+                    dragStartPos = e.Location;
+                    vScrollBarZ.Value = selectedVertex.Z;
+                }
+                pictureBoxRight.Invalidate();
+            }
+        }
+
+        private void pictureBoxRight_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging && selectedVertex != null)
+            {
+                // グリッドにスナップして頂点を移動
+                int newX = e.X / GRID_SIZE * GRID_SIZE;
+                int newY = e.Y / GRID_SIZE * GRID_SIZE;
+
+                // 範囲制限：X=0-200, Y=0-300
+                newX = Math.Max(0, Math.Min(200, newX));
+                newY = Math.Max(0, Math.Min(300, newY));
+
+                selectedVertex.X = newX;
+                selectedVertex.Y = newY;
+
+                pictureBoxRight.Invalidate();
+            }
+        }
+
+        private void pictureBoxRight_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private Vertex FindNearestVertex(Point screenPoint, int maxDistance)
+        {
+            Vertex nearest = null;
+            int minDistanceSquared = maxDistance * maxDistance;
+
+            foreach (var vertex in vertices)
+            {
+                Point screenPos = WorldToScreen(vertex);
+                int dx = screenPoint.X - screenPos.X;
+                int dy = screenPoint.Y - screenPos.Y;
+                int distanceSquared = dx * dx + dy * dy;
+
+                if (distanceSquared < minDistanceSquared)
+                {
+                    minDistanceSquared = distanceSquared;
+                    nearest = vertex;
+                }
+            }
+
+            return nearest;
+        }
+
+        private void vScrollBarZ_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (selectedVertex != null)
+            {
+                selectedVertex.Z = vScrollBarZ.Value;
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InitializeModel();
+            selectedVertex = null;
+            vScrollBarZ.Value = 0;
+        }
+    }
+    // 頂点クラス
+    public class Vertex
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Vertex(int x, int y, int z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    // 三角形クラス
+    public class Triangle
+    {
+        public int V1 { get; set; } // 頂点インデックス
+        public int V2 { get; set; }
+        public int V3 { get; set; }
+
+        public Triangle(int v1, int v2, int v3)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+        }
+    }
+}
