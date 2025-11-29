9c45939c0790436a77bd5514e581e0c8be3f505f
rsna6ce <86136223+rsna6ce@users.noreply.github.com>
2025-11-29 22:27:59 +0900

    updated form design.

--- lowpoly_mask_builder/Form1.Designer.cs
+++ lowpoly_mask_builder/Form1.Designer.cs
@@ -45,12 +45,10 @@ namespace lowpoly_mask_builder
             // 
             // pictureBoxLeft
             // 
-            this.pictureBoxLeft.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBoxLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.pictureBoxLeft.Location = new System.Drawing.Point(4, 28);
             this.pictureBoxLeft.Name = "pictureBoxLeft";
-            this.pictureBoxLeft.Size = new System.Drawing.Size(936, 1009);
+            this.pictureBoxLeft.Size = new System.Drawing.Size(600, 900);
             this.pictureBoxLeft.TabIndex = 0;
             this.pictureBoxLeft.TabStop = false;
             // 
@@ -60,7 +58,7 @@ namespace lowpoly_mask_builder
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1241, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -102,12 +100,10 @@ namespace lowpoly_mask_builder
             // 
             // pictureBoxRight
             // 
-            this.pictureBoxRight.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.pictureBoxRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.pictureBoxRight.Location = new System.Drawing.Point(944, 28);
+            this.pictureBoxRight.Location = new System.Drawing.Point(604, 28);
             this.pictureBoxRight.Name = "pictureBoxRight";
-            this.pictureBoxRight.Size = new System.Drawing.Size(936, 1009);
+            this.pictureBoxRight.Size = new System.Drawing.Size(600, 900);
             this.pictureBoxRight.TabIndex = 0;
             this.pictureBoxRight.TabStop = false;
             this.pictureBoxRight.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxRight_Paint);
@@ -117,9 +113,9 @@ namespace lowpoly_mask_builder
             // 
             // vScrollBarZ
             // 
-            this.vScrollBarZ.Location = new System.Drawing.Point(1880, 28);
+            this.vScrollBarZ.Location = new System.Drawing.Point(1212, 28);
             this.vScrollBarZ.Name = "vScrollBarZ";
-            this.vScrollBarZ.Size = new System.Drawing.Size(20, 780);
+            this.vScrollBarZ.Size = new System.Drawing.Size(20, 900);
             this.vScrollBarZ.TabIndex = 2;
             this.vScrollBarZ.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBarZ_Scroll);
             // 
@@ -127,7 +123,7 @@ namespace lowpoly_mask_builder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(1241, 937);
             this.Controls.Add(this.vScrollBarZ);
             this.Controls.Add(this.pictureBoxRight);
             this.Controls.Add(this.pictureBoxLeft);
@@ -135,8 +131,7 @@ namespace lowpoly_mask_builder
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Text = "Lowpoly mask builder";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
